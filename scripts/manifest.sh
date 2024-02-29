11c9a207a30cbf656d33d860fb14016c099e13b5
valentynbez <walentyn.bzsz@gmail.com>
2024-02-29 17:03:47 +0100

    feat: mock - bacteria

--- scripts/manifest.sh
+++ scripts/manifest.sh
@@ -8,18 +8,18 @@ fi
 input_dir=$1
 output_dir=$2
 
-echo -e "sample-id\tforward-absolute-filepath\treverse-absolute-filepath" > ${output_dir}/manifest.tsv
+echo -e "sample-id\tforward-absolute-filepath\treverse-absolute-filepath" > ${output_dir}/mock-manifest.tsv
 
 for file in $input_dir/*.fastq.gz; do
 
-    sample=$(basename $(echo $file | cut -d'_' -f1))
+    sample=$(basename $(echo $file | cut -d '_' -f1))
     if [[ $file == *"_R1"* ]]; then
         fwd=$(readlink -f $file)
     elif [[ $file == *"_R2"* ]]; then
         rev=$(readlink -f $file)
     fi
     if [[ ! -z $fwd && ! -z $rev ]]; then
-        echo -e "${sample}\t${fwd}\t${rev}" >> ${output_dir}/manifest.tsv
+        echo -e "${sample}\t${fwd}\t${rev}" >> ${output_dir}/mock-manifest.tsv
         fwd=""
         rev=""
     fi
